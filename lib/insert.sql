abce123626a436fd16fd9541757c8bbea73c4dde
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-01 08:46:46 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 2, 'F', 'white', 'average', 1 ),
+('Rowdy', 10, 'M', 'brown', 'extreme', 0) ,
+('Tabitha', 3, 'F', 'black', 'average', 1 ) ,
+('Sergeant Brown', 5, 'M', 'white', 'non-temperamental', 1) ,
+('Melissa', 7, 'F', 'white-black', 'average', 0 ),
+('Grinch', 5, 'M', 'blue', 'extreme', 0 ),
+('Wendy', 9, 'F', 'yellow', 'average', 1 ),
+(NULL, 3, 'M', 'pink', 'non-temperamental', 0);
+
